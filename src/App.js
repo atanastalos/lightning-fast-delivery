8254deff90071017ba1a9468f25250c22e09d8ee
talosati <talos.barbara@gmail.com>
2020-11-08 23:06:41 +0100

    coach

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 
 import logo from './static/logo.png';
 import flaticon from './static/flaticons/flaticon-logo.png';
 import brain from './static/flaticons/brain.png';
 import dev from './static/flaticons/development.png';
 import puzzle from './static/flaticons/puzzle.png';
+import profil from './static/profil.JPG';
 
 function App() {
     return (
@@ -64,6 +65,7 @@ function App() {
             </div>
 
             <div id="agenda">
+                <h2>Agenda</h2>
                 <div className="session">
                     <p>8:30am - Introduction</p>
                     <p>9:00am - Coding time</p>
@@ -96,7 +98,57 @@ function App() {
             </div>
 
             <div id="coach">
-
+                <Container>
+                    <Row>
+                        <Col md={4}>
+                            <img src={profil}/>
+                        </Col>
+                        <Col md={8}>
+                            <h4>Your coach for the day:</h4>
+                            <p>Ivett Ördög is a lead developer and an experienced dev coach who has worked at several
+                                industry
+                                leading companies. She combines her developer and leadership skills with a mindful and
+                                critical
+                                approach to people and is an active participant in meetups, conferences, and the
+                                developer community
+                                in general. She has taught university courses and is an occasional blog author. She is
+                                also the
+                                creator of the Morning Commute YouTube channel.</p>
+                            <p>During her tenure at Emarsys (acquired by SAP) the company has grown from having a dozen
+                                developers
+                                on staff into an engineering organization with over 200 people on payroll. Ivett has
+                                successfully
+                                managed teams during this period. A significant part of the organisation was on boarded
+                                by her as
+                                the leader of the company's bootcamp initiative. She also led the team that was the
+                                first to
+                                implement a continuous delivery approach and she was a strong proponent of using the
+                                Saga pattern
+                                that solved the resiliency issues that arose as the company moved from a monolithic
+                                architecture to
+                                a microservice architecture.</p>
+                            <p>The unique combination of coaching, coding and leadership as well as her experience with
+                                introducing
+                                continuous delivery and microservice architectures makes her a great fit for helping you
+                                get better
+                                at delivering value early and often as the first principle of the Agile manifesto
+                                demands. She is
+                                the perfect opposite of the coach who teaches from the book and believes that a
+                                company's
+                                engineering culture is the key to its success: instead of following a so called “agile
+                                framework” or
+                                hiring “devops” engineers and putting them in a separate silo building a culture of
+                                continuous
+                                improvement, short and incremental iterations and rigorously following a scientific
+                                approach to
+                                problem solving is what makes a successful engineering organization. She lives,
+                                practices, and
+                                teaches agile, lean, and continuous deployment and she's more than happy to share her
+                                knowledge and
+                                comments with you.</p>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
 
             <div id="gallery">
